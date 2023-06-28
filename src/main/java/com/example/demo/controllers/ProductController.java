ff7c9226a15e3856756f1974ef79d408f27a5358
user.name <elberte.reis@salvador.ba.gov.br>
2023-06-28 16:40:00 -0300

    Alteracao controller add metodo PUT

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,7 @@ public class ProductController {
     @Autowired
     ProductRepository productRepository;// injeção de dependẽncia controller se comunicando com repository  
 
+    
     //CRIAR new product    
    @PostMapping("/products") 
     public ResponseEntity<ProductModel> saveProduct (@RequestBody @Valid ProductRecordDto productRecordDto){//assinatura do metodo retornando uma entidade de retorno do tipo model, recebendo com parametro o dto para verificação de integridade junto com @requestbody  
@@ -34,23 +36,40 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));// por fim chegou o return, antes de retornar ele salvará o produto model no repository e depois retornará status OK
     }
 
+    
     //BUSCAR all Products
     @GetMapping("/products") //definir uri
     public ResponseEntity<List<ProductModel>> getAllProducts(){//Assinatura sera composta de entidade de resposta + lista de model
         return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll()); //return entidade de rersposta + status OK e corpo de repository.findAll
     }
 
-    //BUSCAR one products
+    
+     //BUSCAR one products
     @GetMapping ("/products/{id}")//busca por id temos que colocar o tipo na uri, nesse caso será uma busca por {id}, lembrar de deixar entre chaves
     public ResponseEntity<Object> getOneProduct(@PathVariable (value = "id") UUID id){//assinatura vai retornar um Object pq existirá tratamento de resposta, paramentro @pathvariable para consegui pegar o id que está dentro da uri, id e tipo
        Optional<ProductModel> product0 = productRepository.findById(id); //ir no banco de dados fazer uma busca através de um metodo JPA findById, passamos o id pra ele e ele vai na base fazer o select
                                                                          // Por fim o product0 criado para receber esse objeto caso ele exista
         if(product0.isEmpty()){ // depois por ultimo fazemos um tratamento utlizando do HttpStatus: se produto for vazio isEMpty = notfound senao OK
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto inesistente.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto inexistente.");
        }
        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
-
     }
+    
+    
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProduct (@PathVariable (value = "id") UUID id, 
+                                                                @RequestBody  @Valid ProductRecordDto productRecordDto ){
+        Optional<ProductModel>  product0 = productRepository.findById(id);
+        if(product0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto inexistente");
+        }
+        ProductModel productModel =product0.get();
+        
+        BeanUtils.copyProperties(productRecordDto, productModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
+    }
+
     
     
 
